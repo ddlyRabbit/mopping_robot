4f64ac29ad6c48e4ab1c62e92209ab7d7d949922
Abhinav Roy <abhinavroy@pop-os.localdomain>
2020-07-14 16:16:25 +0530

    Arduino code added

---
+++ arduino/sketch_jun20a.ino
@@ -0,0 +1,63 @@
+#include <Arduino.h>
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+
+#define led_pin 13
+#define step_pin_right 4
+#define direction_pin_right 3
+#define step_pin_left 8
+#define direction_pin_left 7
+#define enable_pin 6
+
+AccelStepper right_motor(AccelStepper::DRIVER, step_pin_right, direction_pin_right);
+AccelStepper left_motor(AccelStepper::DRIVER, step_pin_left, direction_pin_left);
+MultiStepper steppers;
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(10);
+  pinMode(led_pin, OUTPUT);
+  right_motor.setEnablePin(enable_pin);
+  right_motor.setPinsInverted(false, false, true);  right_motor.setMaxSpeed(36000);
+  right_motor.setMaxSpeed(3600);
+  right_motor.setSpeed(1080);
+//  right_motor.setAcceleration(1200);
+  left_motor.setMaxSpeed(3600);
+  left_motor.setSpeed(1080);
+//  left_motor.setAcceleration(1200);
+  right_motor.enableOutputs();
+  left_motor.enableOutputs();
+
+//  steppers.addStepper(right_motor);
+//  steppers.addStepper(left_motor);
+}
+
+int extract_angle(String cmd){
+  int len = cmd.length();
+  String angle_str = cmd.substring(1,len);
+  int angle = angle_str.toInt();
+  return angle;
+}
+
+void loop() {
+  left_motor.runSpeed();
+  right_motor.runSpeed();
+  if(Serial.available() > 0)
+  { 
+    String command = Serial.readStringUntil('\n');
+    Serial.println(command);  
+    if(command[0] == 'T'){
+      digitalWrite(led_pin, HIGH);
+      int move_angle = extract_angle(command);
+      left_motor.setSpeed(move_angle);
+      right_motor.setSpeed(move_angle);
+      Serial.println("Moving by --> " + String(move_angle) + " degrees");
+    }
+
+    if(command[0] == 'S'){
+      digitalWrite(led_pin, LOW);
+    }
+  }
+  // put your main code here, to run repeatedly:
+}
