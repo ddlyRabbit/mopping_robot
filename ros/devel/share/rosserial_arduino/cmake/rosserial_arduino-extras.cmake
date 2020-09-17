83dda6c7e0f0c26e02a83afb7b838c4b4f69bef8
roy <roy@pop-os.localdomain>
2020-09-17 14:30:34 +0530

    wip

--- ros/devel/share/rosserial_arduino/cmake/rosserial_arduino-extras.cmake
+++
@@ -1,4 +0,0 @@
-cmake_minimum_required(VERSION 3.7.2)
-
-set(ROSSERIAL_ARDUINO_TOOLCHAIN "/home/abhinavroy/robotics_learn/mopping_robot/ros/src/rosserial/rosserial_arduino/arduino-cmake/cmake/ArduinoToolchain.cmake")
-
--- ros/devel/share/rosserial_tivac/cmake/rosserial_tivac-extras.cmake
+++
@@ -1,3 +0,0 @@
-cmake_minimum_required(VERSION 3.7.2)
-
-set(ROSSERIAL_TIVAC_TOOLCHAIN "/home/abhinavroy/robotics_learn/mopping_robot/ros/src/rosserial/rosserial_tivac/tivac-cmake/cmake/TivaCToolchain.cmake")
